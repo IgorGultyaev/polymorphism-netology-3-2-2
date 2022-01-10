012bea7a11138bf3569a49e4c26eb56df94cfac4
asp-gin <igor@gultyaev.ru>
2022-01-10 08:53:06 +0300

    Initial commit

---
+++ src/com/company/IncomeTaxType.java
@@ -0,0 +1,5 @@
+package com.company;
+
+public class IncomeTaxType extends TaxType{
+    // TODO Подоходный налог, = 13%
+}
---
+++ src/com/company/ProgressiveTaxType.java
@@ -0,0 +1,5 @@
+package com.company;
+
+public class ProgressiveTaxType extends TaxType{
+    //TODO НДС, = 18%
+}
